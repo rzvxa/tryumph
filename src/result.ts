5415b05dc954cad6c98c567215da471628bae3c5
rzvxa <rzvxa@protonmail.com>
2023-12-25 08:50:46 +0330

    refactor: switch to class instead of closures

--- src/result.ts
+++ src/result.ts
@@ -1,77 +1,69 @@
-type Result<TResult, TError> = {
-  isOk: () => boolean;
-  isErr: () => boolean;
-  unwrap: () => TResult;
-  unwrapErr: () => TError;
-  unwrapOr: (defaultResult: TResult) => TResult;
-  unwrapOrElse: (defaultProvider: (error: TError) => TResult) => TResult;
-  ok: () => TResult | undefined;
-  error: () => TError | undefined;
-};
+class Result<TResult, TError> {
+  #result: TResult | undefined;
+  #error: TError | undefined;
 
-function makeResult<TResult, TError>({
-  result = undefined,
-  error = undefined,
-}: {
-  result?: TResult | undefined;
-  error?: TError | undefined;
-}): Result<TResult, TError> {
-  if (result === undefined && error === undefined) {
-    throw Error(
-      "No result nor error where provided, You should provide at least one in order to make a result"
-    );
-  }
-  if (result && error) {
-    throw Error(
-      "Both result and error where provided, You should only pass one to the constructor"
-    );
+  constructor({
+    result = undefined,
+    error = undefined,
+  }: {
+    result?: TResult | undefined;
+    error?: TError | undefined;
+  }) {
+    if (result === undefined && error === undefined) {
+      throw Error(
+        "No result nor error where provided, You should provide at least one in order to make a result"
+      );
+    }
+    if (result && error) {
+      throw Error(
+        "Both result and error where provided, You should only pass one to the constructor"
+      );
+    }
+
+    this.#result = result;
+    this.#error = error;
   }
 
-  const isOk = () => result !== undefined;
-  const isErr = () => error !== undefined;
-
-  return {
-    isOk,
-    isErr,
-    unwrap: () => {
-      if (isOk()) {
-        return result!;
-      } else {
-        throw error;
-      }
-    },
-    unwrapErr: () => {
-      if (isErr()) {
-        return error!;
-      } else {
-        throw result;
-      }
-    },
-    unwrapOr: (defaultResult: TResult) => {
-      if (isErr()) {
-        return defaultResult;
-      } else {
-        return result!;
-      }
-    },
-    unwrapOrElse: (defaultProvider: (error: TError) => TResult) => {
-      if (isErr()) {
-        return defaultProvider(error!);
-      } else {
-        return result!;
-      }
-    },
-    ok: () => result,
-    error: () => error,
+  isOk = () => this.#result !== undefined;
+  isErr = () => this.#error !== undefined;
+  unwrap = () => {
+    if (this.isOk()) {
+      return this.#result!;
+    } else {
+      throw this.#error;
+    }
   };
+  unwrapErr = () => {
+    if (this.isErr()) {
+      return this.#error!;
+    } else {
+      throw this.#result;
+    }
+  };
+  unwrapOr = (defaultResult: TResult) => {
+    if (this.isErr()) {
+      return defaultResult;
+    } else {
+      return this.#result!;
+    }
+  };
+  unwrapOrElse = (defaultProvider: (error: TError) => TResult) => {
+    if (this.isErr()) {
+      return defaultProvider(this.#error!);
+    } else {
+      return this.#result!;
+    }
+  };
+  ok = () => this.#result;
+  error = () => this.#error;
 }
 
 function Ok<TResult, TError>(result: TResult): Result<TResult, TError> {
-  return makeResult({ result });
+  return new Result({ result });
 }
 
 function Err<TResult, TError>(error: TError): Result<TResult, TError> {
-  return makeResult({ error });
+  return new Result({ error });
 }
 
 export type { Result };
