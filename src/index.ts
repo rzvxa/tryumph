5b60c88032b265358f975ecd7a6bd2a5535f929a
rzvxa <rzvxa@protonmail.com>
2023-12-27 02:35:55 +0330

    fix: issues in renaming

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ console.error(
 import type { Matcher } from "./matcher";
 import type { Result } from "./result";
 import { Ok, Err } from "./result";
-import tryAsync from "./tryAsync";
+import try$ from "./try";
 import when from "./when";
 
 export type { Matcher, Result };
-export { Ok, Err, tryAsync, when };
+export { Ok, Err, try$, when };
