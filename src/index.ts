18e3c2da8236ff80a994e0991feacf7ac5142314
rzvxa <rzvxa@protonmail.com>
2023-12-27 04:15:10 +0330

    feat: exposed tryFn$ in index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import type { Matcher } from "./matcher";
 import type { Result } from "./result";
 import { Ok, Err } from "./result";
 import try$ from "./try";
+import tryFn$ from "./tryFn";
 import when from "./when";
 
 export type { Matcher, Result };
-export { Ok, Err, try$, when };
+export { Ok, Err, try$, tryFn$, when };
