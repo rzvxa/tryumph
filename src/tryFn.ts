db99b34dd781fbdeb1ab028c02f45a5a344678a5
rzvxa <rzvxa@protonmail.com>
2023-12-27 04:41:21 +0330

    feat: used tryFn$ for sync opetations

--- src/tryFn.ts
+++ src/tryFn.ts
@@ -3,12 +3,12 @@ import { Ok, Err } from "./result";
 
 type AnyFunction = (...args: any[]) => any;
 
-async function tryFn$<TError, TFunc extends AnyFunction>(
+function tryFn$<TError, TFunc extends AnyFunction>(
   fn: TFunc,
   ...args: Parameters<TFunc>
-): Promise<Result<ReturnType<TFunc>, TError>> {
+): Result<ReturnType<TFunc>, TError> {
   try {
-    const result: ReturnType<TFunc> = await fn(...args);
+    const result: ReturnType<TFunc> = fn(...args);
     return Ok(result);
   } catch (err) {
     return Err(err as TError);
--- tests/tryFn.spec.ts
+++ tests/tryFn.spec.ts
@@ -1,45 +1,38 @@
-import try$ from "../src/try";
 import tryFn$ from "../src/tryFn";
 
 describe("tryFn$ Tests", () => {
   const resolveMock = jest.fn(
-    async (
-      a: number,
-      b: string,
-      c: boolean
-    ): Promise<[number, string, boolean]> => [a, b, c]
+    (a: number, b: string, c: boolean): [number, string, boolean] => [a, b, c]
   );
-  const rejectMock = jest
-    .fn(async (a: number, b: string, c: boolean) => [a, b, c])
-    .mockRejectedValue("ERR");
+  const rejectMock = jest.fn(
+    (_a: number, _b: string, _c: boolean): [number, string, boolean] => {
+      throw "ERR";
+    }
+  );
   const a = 1;
   const b = "1";
   const c = true;
-  test("should not throw error on resolving faulty promises", async () => {
-    await expect(tryFn$(rejectMock, a, b, c)).resolves.toBeTruthy();
+  test("should not throw error on execution of a faulty function", () => {
+    expect(tryFn$(rejectMock, a, b, c)).toBeTruthy();
   });
 
-  test("should resolve on promises that resolve", async () => {
-    await expect(tryFn$(resolveMock, a, b, c)).resolves.toBeTruthy();
+  test("should return on functions that also return", () => {
+    expect(tryFn$(resolveMock, a, b, c)).toBeTruthy();
   });
 
-  test("should return a ok result that reports isOk", async () => {
-    const result = await tryFn$(resolveMock, a, b, c);
-    expect(result.isOk()).toEqual(true);
+  test("should return a ok result that reports isOk", () => {
+    expect(tryFn$(resolveMock, a, b, c).isOk()).toEqual(true);
   });
 
-  test("should return a ok result that can unwrap [a, b, c] value", async () => {
-    const result = await tryFn$(resolveMock, a, b, c);
-    expect(result.unwrap()).toEqual([a, b, c]);
+  test("should return a ok result that can unwrap [a, b, c] value", () => {
+    expect(tryFn$(resolveMock, a, b, c).unwrap()).toEqual([a, b, c]);
   });
 
-  test("should return a ok result that does not report isErr", async () => {
-    const result = await tryFn$(resolveMock, a, b, c);
-    expect(result.isErr()).toEqual(false);
+  test("should return a ok result that does not report isErr", () => {
+    expect(tryFn$(resolveMock, a, b, c).isErr()).toEqual(false);
   });
 
-  test("should return a err result that has 'ERR' as error value", async () => {
-    const result = await tryFn$(rejectMock, a, b, c);
-    expect(result.error()).toEqual("ERR");
+  test("should return a err result that has 'ERR' as error value", () => {
+    expect(tryFn$(rejectMock, a, b, c).error()).toEqual("ERR");
   });
 });
