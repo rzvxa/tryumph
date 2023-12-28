121f71686c18d46adbf8c33e440853f45fcb2230
rzvxa <rzvxa@protonmail.com>
2023-12-28 07:00:31 +0330

    chore: remove dist from semantic release

--- dist/tupleConstructor.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * A base type to extend in order to mimic the tuple types behavior.
- */
-declare const TupleConstructor: new <T, Y>(...p: [T | null, Y | null]) => [
-    T | null,
-    Y | null
-];
-export default TupleConstructor;
-//# sourceMappingURL=tupleConstructor.d.ts.map
--- dist/tupleConstructor.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * A base type to extend in order to mimic the tuple types behavior.
- */
-const TupleConstructor = Array; // eslint-disable-line @typescript-eslint/no-explicit-any
-exports.default = TupleConstructor;
-//# sourceMappingURL=tupleConstructor.js.map
